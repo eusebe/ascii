0f0f2c54904e8a78dd9ea6b32842b21aa7973c0c
eusebe <dhajage@gmail.com>
2009-01-19 14:05:37 +0100

    improve caption in header generator

--- misc.r
+++ misc.r
@@ -1,5 +1,5 @@
 # generate column specifiers
-cols <- function(ncol, align = "l", width = 1, style = "d", multiplier = 1) {
+cols <- function(ncol, align = "l", col.width = 1, style = "d", multiplier = 1) {
 
   if (multiplier > 1) {
     ncol <- 1
@@ -13,21 +13,21 @@ cols <- function(ncol, align = "l", width = 1, style = "d", multiplier = 1) {
   style <- unlist(strsplit(style, ""))
 
   align <- rep(align, length.out = ncol)
-  width <- rep(width, length.out = ncol)
+  col.width <- rep(col.width, length.out = ncol)
   style <- rep(style, length.out = ncol)
   
   align[align == "l"] <- "<"
   align[align == "c"] <- "^"
   align[align == "r"] <- ">"
 
-  res <- paste(multiplier, align, width, style, collapse = ",", sep = "")
+  res <- paste(multiplier, align, col.width, style, collapse = ",", sep = "")
   return(res)
 }
 #~ cols(ncol = 3, align = "llrclr")
 #~ cols(ncol = 3, align = "llrclr", multiplier = 3)
 
 # generate headers
-header <- function(caption = "", frame = "", grid = "", valign = "", options = "header", cols = "", width = "") {
+header <- function(caption = "", frame = "", grid = "", valign = "", options = "", cols = "", width = "") {
 
   if (frame != "") frame <- paste('frame="', frame, '"', sep = "")
   if (grid != "") grid <- paste('grid="', grid, '"', sep = "")
@@ -39,10 +39,16 @@ header <- function(caption = "", frame = "", grid = "", valign = "", options = "
   listarg <- c(frame, grid, valign, options, cols, width)
   listarg <- listarg[listarg != ""]
 
-  if (caption == "") res <- paste("[", paste(listarg, collapse = ","), "]", sep = "")
-  else               res <- paste(".", caption, "\n", "[", paste(listarg, collapse = ","), "]", sep = "")
+  if (length(listarg) != 0) {
+    res <- paste("[", paste(listarg, collapse = ","), "]\n", sep = "")
+  }
+  else res <- ""
+  if (caption != "") res <- paste(".", caption, "\n", res, sep = "")
   return(res)
 }
-header(frame = "none")
-header(frame = "none", cols = cols(ncol = 3, align = "llrclr"))
-header(caption = "A title", frame = "none", cols = cols(ncol = 3, align = "llrclr"))
+#~ cat(header())
+#~ cat(header(frame = "none"))
+#~ cat(header(frame = "none", cols = cols(ncol = 3, align = "llrclr")))
+#~ cat(header(caption = "A title", frame = "none", cols = cols(ncol = 3, align = "llrclr")))
+#~ cat(header(caption = "A title"))
+
