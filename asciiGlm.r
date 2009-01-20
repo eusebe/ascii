4b6a5c49b51396ad8590c25671eee7aabd7f1dcd
eusebe <dhajage@gmail.com>
2009-01-20 14:26:06 +0100

    method for lm and glm class object

---
+++ asciiGlm.r
@@ -0,0 +1,15 @@
+setMethod(
+  "ascii",
+  "glm",
+  function (x, include.rownames = TRUE, include.colnames = TRUE, format = "f", digits = 2, decimal.mark = ".", na.print = "", caption = "", width = 0, frame = "", grid = "", valign = "", header = TRUE, footer = FALSE, align = "", col.width = 1, style = ""){
+    x <- as.data.frame(summary(x)$coef)
+    new("R2asciidocDataFrame", x = x, include.rownames = include.rownames,
+         include.colnames = include.colnames, format = format,
+         digits = digits, decimal.mark = decimal.mark, na.print = na.print,
+         caption = caption, width = width, frame = frame, grid = grid,
+         valign = valign, header = header, footer = footer, align = align,
+         col.width = col.width, style = style)
+  }
+)
+
+
