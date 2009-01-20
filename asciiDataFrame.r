dee376500c767b72d2dbd61c5a2a26b61d5fc607
eusebe <dhajage@gmail.com>
2009-01-20 14:36:35 +0100

    method for summary.lm, summary.glm and cast_df (reshape) class object

--- asciiDataFrame.r
+++ asciiDataFrame.r
@@ -11,18 +11,17 @@ setMethod(
   }
 )
 
-# from package reshape (Hadley Wickham : http://had.co.nz/reshape/)
-#~ setMethod(
-#~   "ascii",
-#~   "cast_df",
-#~   function (x, include.rownames = TRUE, include.colnames = TRUE, format = "f", digits = 2, decimal.mark = ".", na.print = ""){
-#~     new("R2asciidocDataFrame", x = x, include.rownames = include.rownames,
-#~          include.colnames = include.colnames, format = format,
-#~          digits = digits, decimal.mark = decimal.mark, na.print = na.print)
-#~   }
-#~ )
-
-## FIXME cast_df:
-#~ Warning message:
-#~ In matchSignature(signature, fdef, where) :
-#~   in the method signature for function "ascii" no definition for class: “cast_df”
+#~ # from package reshape (Hadley Wickham : http://had.co.nz/reshape/)
+setMethod(
+  "ascii",
+  "cast_df",
+  function (x, include.rownames = FALSE, include.colnames = TRUE, format = "f", digits = 2, decimal.mark = ".", na.print = "", caption = "", width = 0, frame = "", grid = "", valign = "", header = TRUE, footer = FALSE, align = "", col.width = 1, style = ""){
+    x <- as.data.frame(x)
+    new("R2asciidocDataFrame", x = x, include.rownames = include.rownames,
+         include.colnames = include.colnames, format = format,
+         digits = digits, decimal.mark = decimal.mark, na.print = na.print,
+         caption = caption, width = width, frame = frame, grid = grid,
+         valign = valign, header = header, footer = footer, align = align,
+         col.width = col.width, style = style)
+  }
+)
