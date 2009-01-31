856799569144e54cb53db137a59c054e8e4c9b90
eusebe <dhajage@gmail.com>
2009-01-31 22:43:03 +0100

    New density and anova method, improve show for list

---
+++ asciiAnova.r
@@ -0,0 +1,14 @@
+setMethod(
+  "ascii",
+  "anova",
+  function (x, include.rownames = FALSE, include.colnames = TRUE, format = "f", digits = 2, decimal.mark = ".", na.print = "", caption = "", width = 0, frame = "", grid = "", valign = "", header = TRUE, footer = FALSE, align = "", col.width = 1, style = ""){
+    x <- data.frame(Df = x$Df, "Sum Sq" = x$"Sum Sq", "Mean Sq" = x$"Mean Sq", "F value" = x$"F value", " Pr(>F)" = x$"Pr(>F)", check.names = FALSE)
+    new("R2asciidocDataFrame", x = x, include.rownames = include.rownames,
+         include.colnames = include.colnames, format = format,
+         digits = digits, decimal.mark = decimal.mark, na.print = na.print,
+         caption = caption, width = width, frame = frame, grid = grid,
+         valign = valign, header = header, footer = footer, align = align,
+         col.width = col.width, style = style)
+  }
+)
+
