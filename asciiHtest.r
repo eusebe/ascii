dd434a20caae49259adc88bb5652a912a9cc4ddb
eusebe <dhajage@gmail.com>
2009-02-02 20:24:07 +0100

    method for htest + improve survdiff

---
+++ asciiHtest.r
@@ -0,0 +1,27 @@
+setMethod(
+  "ascii",
+  "htest",
+  function (x, include.rownames = TRUE, include.colnames = TRUE, format = "f", digits = 2, decimal.mark = ".", na.print = "", caption = "", width = 0, frame = "", grid = "", valign = "", header = TRUE, footer = FALSE, align = "", col.width = 1, style = ""){
+  if (x$method == "Fisher's Exact Test for Count Data") {
+    res <- cbind(x$estimate, x$conf.int[1], x$conf.int[2], x$p.value)
+    colnames(res) <- c("Odds ratio", "lower .95", "upper .95", "p-value")
+  }
+  if (x$method == "Pearson's Chi-squared test with Yates' continuity correction" | x$method == "Pearson's Chi-squared test"){
+    res <- cbind(x$statistic, x$parameter, x$p.value)
+    colnames(res) <- c("X-squared", "df", "p-value")
+  }
+  ascii(res, include.rownames = include.rownames,
+         include.colnames = include.colnames, format = format,
+         digits = digits, decimal.mark = decimal.mark, na.print = na.print,
+         caption = caption, width = width, frame = frame, grid = grid,
+         valign = valign, header = header, footer = footer, align = align,
+         col.width = col.width, style = style)
+
+#  new("R2asciidocDataFrame", x = res, include.rownames = include.rownames,
+#         include.colnames = include.colnames, format = format,
+#         digits = digits, decimal.mark = decimal.mark, na.print = na.print,
+#         caption = caption, width = width, frame = frame, grid = grid,
+#         valign = valign, header = header, footer = footer, align = align,
+#         col.width = col.width, style = style)
+  }
+)
