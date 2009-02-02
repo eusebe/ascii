28c2806fdf65ee6a1d1710d2299b9c3b233e5870
eusebe <dhajage@gmail.com>
2009-02-02 20:29:49 +0100

    method for htest + improve survdiff

--- asciiSurvdiff.r
+++ asciiSurvdiff.r
@@ -2,19 +2,38 @@ setMethod(
   "ascii",
   "survdiff",
   function (x, include.rownames = TRUE, include.colnames = TRUE, format = "f", digits = 2, decimal.mark = ".", na.print = "", caption = "", width = 0, frame = "", grid = "", valign = "", header = TRUE, footer = FALSE, align = "", col.width = 1, style = ""){
+    # From print.survdiff
+    omit <- x$na.action
+    if (length(omit))
+        cat("n=", sum(x$n), ", ", naprint(omit), ".\n\n", sep = "")
+    if (length(x$n) == 1) {
+        z <- sign(x$exp - x$obs) * sqrt(x$chisq)
+        temp <- c(x$obs, x$exp, z, signif(1 - pchisq(x$chisq,
+            1), digits))
+        names(temp) <- c("Observed", "Expected", "Z", "p")
+        temp <- t(temp)
+        include.rownames = FALSE
+    }
+    else {
+        if (is.matrix(x$obs)) {
+            otmp <- apply(x$obs, 1, sum)
+            etmp <- apply(x$exp, 1, sum)
+        }
+        else {
+            otmp <- x$obs
+            etmp <- x$exp
+        }
+        df <- c((sum(1 * (etmp > 0))) - 1, rep(NA, length(n) - 1))
+        p <- c(1 - pchisq(x$chisq, df), rep(NA, length(n) - 1))
+        temp <- cbind(x$n, otmp, etmp, ((otmp - etmp)^2)/etmp,
+            ((otmp - etmp)^2)/diag(x$var), df, p)
+        dimnames(temp) <- list(names(x$n), c("N", "Observed",
+            "Expected", "(O-E)^2/E", "(O-E)^2/V", "df", "p"))
+    }
 
-  N <- x[[1]]
-  n <- length(N)
-  df <- c(n, rep(NA, n-1))
-  obs <- x[[2]]
-  exp <- x[[3]]
-  var <- diag(x$var)
-  c <- c(x$chisq, rep(NA, n-1))
-  p <- c(1-pchisq(x$chisq, n-1), rep(NA, n-1))
-  tmp <- cbind(N, obs, exp, (obs-exp)^2/exp, (obs-exp)^2/var, c, df, p)
-  dimnames(tmp) <- list(names(N), c("N", "Obs", "Exp", "(O-E)^2/E", "(O-E)^2/V", "Chisq", "df", "p"))
-  tmp <- as.data.frame(tmp, checknames = FALSE)
-  new("R2asciidocDataFrame", x = tmp, include.rownames = include.rownames,
+  temp <- as.data.frame(temp, checknames = FALSE)
+
+  new("R2asciidocDataFrame", x = temp, include.rownames = include.rownames,
       include.colnames = include.colnames, format = format,
       digits = digits, decimal.mark = decimal.mark, na.print = na.print,
       caption = caption, width = width, frame = frame, grid = grid,
