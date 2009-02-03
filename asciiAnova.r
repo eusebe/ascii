7aa649bdcd30a95099c6e37641df82ea553779c2
eusebe <dhajage@gmail.com>
2009-02-03 15:09:37 +0100

    Fix bug with asciiAnova

--- asciiAnova.r
+++ asciiAnova.r
@@ -2,7 +2,7 @@ setMethod(
   "ascii",
   "anova",
   function (x, include.rownames = FALSE, include.colnames = TRUE, format = "f", digits = 2, decimal.mark = ".", na.print = "", caption = "", width = 0, frame = "", grid = "", valign = "", header = TRUE, footer = FALSE, align = "", col.width = 1, style = ""){
-    x <- data.frame(Df = x$Df, "Sum Sq" = x$"Sum Sq", "Mean Sq" = x$"Mean Sq", "F value" = x$"F value", " Pr(>F)" = x$"Pr(>F)", check.names = FALSE)
+    x <- as.data.frame(x, check.names = FALSE)
     new("R2asciidocDataFrame", x = x, include.rownames = include.rownames,
          include.colnames = include.colnames, format = format,
          digits = digits, decimal.mark = decimal.mark, na.print = na.print,
