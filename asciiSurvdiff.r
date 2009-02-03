0c37341bcd20f99e9e0406d08ea43c6d5013a29c
eusebe <dhajage@gmail.com>
2009-02-03 15:25:06 +0100

    fix bug with asciiSurvDiff

--- asciiSurvdiff.r
+++ asciiSurvdiff.r
@@ -3,9 +3,6 @@ setMethod(
   "survdiff",
   function (x, include.rownames = TRUE, include.colnames = TRUE, format = "f", digits = 2, decimal.mark = ".", na.print = "", caption = "", width = 0, frame = "", grid = "", valign = "", header = TRUE, footer = FALSE, align = "", col.width = 1, style = ""){
     # From print.survdiff
-    omit <- x$na.action
-    if (length(omit))
-        cat("n=", sum(x$n), ", ", naprint(omit), ".\n\n", sep = "")
     if (length(x$n) == 1) {
         z <- sign(x$exp - x$obs) * sqrt(x$chisq)
         temp <- c(x$obs, x$exp, z, signif(1 - pchisq(x$chisq,
@@ -13,18 +10,16 @@ setMethod(
         names(temp) <- c("Observed", "Expected", "Z", "p")
         temp <- t(temp)
         include.rownames = FALSE
-    }
-    else {
+    } else {
         if (is.matrix(x$obs)) {
             otmp <- apply(x$obs, 1, sum)
             etmp <- apply(x$exp, 1, sum)
-        }
-        else {
+        } else {
             otmp <- x$obs
             etmp <- x$exp
         }
-        df <- c((sum(1 * (etmp > 0))) - 1, rep(NA, length(n) - 1))
-        p <- c(1 - pchisq(x$chisq, df), rep(NA, length(n) - 1))
+        df <- c((sum(1 * (etmp > 0))) - 1, rep(NA, length(x$n) - 1))
+        p <- c(1 - pchisq(x$chisq, df[!is.na(df)]), rep(NA, length(x$n) - 1))
         temp <- cbind(x$n, otmp, etmp, ((otmp - etmp)^2)/etmp,
             ((otmp - etmp)^2)/diag(x$var), df, p)
         dimnames(temp) <- list(names(x$n), c("N", "Observed",
