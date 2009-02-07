ad4d93786dbe4e9382675614861e9f464b188e86
eusebe <dhajage@gmail.com>
2009-02-07 18:50:00 +0100

    Add method for chisq test for given probability

--- asciiHtest.r
+++ asciiHtest.r
@@ -6,7 +6,7 @@ setMethod(
     res <- cbind(x$estimate, x$conf.int[1], x$conf.int[2], x$p.value)
     colnames(res) <- c("Odds ratio", "lower .95", "upper .95", "p-value")
   }
-  if (x$method == "Pearson's Chi-squared test with Yates' continuity correction" | x$method == "Pearson's Chi-squared test"){
+  if (x$method == "Pearson's Chi-squared test with Yates' continuity correction" | x$method == "Pearson's Chi-squared test" | x$method == "Chi-squared test for given probabilities"){
     res <- cbind(x$statistic, x$parameter, x$p.value)
     colnames(res) <- c("X-squared", "df", "p-value")
   }
@@ -16,12 +16,5 @@ setMethod(
          caption = caption, width = width, frame = frame, grid = grid,
          valign = valign, header = header, footer = footer, align = align,
          col.width = col.width, style = style)
-
-#  new("R2asciidocDataFrame", x = res, include.rownames = include.rownames,
-#         include.colnames = include.colnames, format = format,
-#         digits = digits, decimal.mark = decimal.mark, na.print = na.print,
-#         caption = caption, width = width, frame = frame, grid = grid,
-#         valign = valign, header = header, footer = footer, align = align,
-#         col.width = col.width, style = style)
   }
 )
