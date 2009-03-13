84fff4cd9736bac011092ee249fec1a972559dc9
eusebe <dhajage@gmail.com>
2009-03-13 16:57:56 +0100

    ascii for aov object

--- asciiAnova.r
+++ asciiAnova.r
@@ -10,3 +10,62 @@ ascii.anova <- function (x, include.rownames = TRUE, include.colnames = TRUE, fo
     return(obj)
 }
 
+ascii.aov <- function (x, include.rownames = TRUE, include.colnames = TRUE, format = "f", digits = 2, decimal.mark = ".", na.print = "", caption = "", width = 0, frame = "", grid = "", valign = "", header = TRUE, footer = FALSE, align = "", col.width = 1, style = ""){
+  ascii.anova(unclass(summary(x))[[1]], include.rownames = include.rownames,
+         include.colnames = include.colnames, format = format,
+         digits = digits, decimal.mark = decimal.mark, na.print = na.print,
+         caption = caption, width = width, frame = frame, grid = grid,
+         valign = valign, header = header, footer = footer, align = align,
+         col.width = col.width, style = style)
+}
+
+ascii.summary.aov <- function (x, include.rownames = TRUE, include.colnames = TRUE, format = "f", digits = 2, decimal.mark = ".", na.print = "", caption = "", width = 0, frame = "", grid = "", valign = "", header = TRUE, footer = FALSE, align = "", col.width = 1, style = ""){
+  ascii.anova(unclass(x)[[1]], include.rownames = include.rownames,
+         include.colnames = include.colnames, format = format,
+         digits = digits, decimal.mark = decimal.mark, na.print = na.print,
+         caption = caption, width = width, frame = frame, grid = grid,
+         valign = valign, header = header, footer = footer, align = align,
+         col.width = col.width, style = style)
+}
+
+ascii.aovlist <- function (x, include.rownames = TRUE, include.colnames = TRUE, format = "f", digits = 2, decimal.mark = ".", na.print = "", caption = "", width = 0, frame = "", grid = "", valign = "", header = TRUE, footer = FALSE, align = "", col.width = 1, style = ""){
+  y <- summary(x)
+  n <- length(y)
+  if (n == 1) ascii.anova(unclass(y[[1]][[1]]), include.rownames = include.rownames,
+         include.colnames = include.colnames, format = format,
+         digits = digits, decimal.mark = decimal.mark, na.print = na.print,
+         caption = caption, width = width, frame = frame, grid = grid,
+         valign = valign, header = header, footer = footer, align = align,
+         col.width = col.width, style = style)
+  else {
+    z <- y[[1]][[1]]
+    for (i in 2:n) z <- rbind(z, y[[i]][[1]])
+    ascii.anova(z, include.rownames = include.rownames,
+         include.colnames = include.colnames, format = format,
+         digits = digits, decimal.mark = decimal.mark, na.print = na.print,
+         caption = caption, width = width, frame = frame, grid = grid,
+         valign = valign, header = header, footer = footer, align = align,
+         col.width = col.width, style = style)
+  }
+}
+
+ascii.summary.aovlist <- function (x, include.rownames = TRUE, include.colnames = TRUE, format = "f", digits = 2, decimal.mark = ".", na.print = "", caption = "", width = 0, frame = "", grid = "", valign = "", header = TRUE, footer = FALSE, align = "", col.width = 1, style = ""){
+  n <- length(x)
+  if (n == 1) ascii.anova(unclass(x[[1]][[1]]), include.rownames = include.rownames,
+         include.colnames = include.colnames, format = format,
+         digits = digits, decimal.mark = decimal.mark, na.print = na.print,
+         caption = caption, width = width, frame = frame, grid = grid,
+         valign = valign, header = header, footer = footer, align = align,
+         col.width = col.width, style = style)
+  else {
+    z <- x[[1]][[1]]
+    for (i in 2:n) z <- rbind(z, x[[i]][[1]])
+    ascii.anova(z, include.rownames = include.rownames,
+         include.colnames = include.colnames, format = format,
+         digits = digits, decimal.mark = decimal.mark, na.print = na.print,
+         caption = caption, width = width, frame = frame, grid = grid,
+         valign = valign, header = header, footer = footer, align = align,
+         col.width = col.width, style = style)
+  }
+}
+
