0d8e683d5686d49f595af41451146ae3c9a2d55c
eusebe <dhajage@gmail.com>
2009-03-13 15:35:29 +0100

    include.rownames = T for anova object

--- asciiAnova.r
+++ asciiAnova.r
@@ -1,4 +1,4 @@
-ascii.anova <- function (x, include.rownames = FALSE, include.colnames = TRUE, format = "f", digits = 2, decimal.mark = ".", na.print = "", caption = "", width = 0, frame = "", grid = "", valign = "", header = TRUE, footer = FALSE, align = "", col.width = 1, style = ""){
+ascii.anova <- function (x, include.rownames = TRUE, include.colnames = TRUE, format = "f", digits = 2, decimal.mark = ".", na.print = "", caption = "", width = 0, frame = "", grid = "", valign = "", header = TRUE, footer = FALSE, align = "", col.width = 1, style = ""){
     x <- as.data.frame(x, check.names = FALSE)
     obj <- asciiDataFrame$new(x = x, include.rownames = include.rownames,
          include.colnames = include.colnames, format = format,
