35e6b2653c5d4a1097a119968e1a33354fc573e2
eusebe <dhajage@gmail.com>
2009-03-16 15:04:45 +0100

    add man pages

---
+++ help.r
@@ -0,0 +1,7 @@
+# all S3method
+methods("ascii") -> meth
+cat(paste("S3method(\"ascii\", \"", meth, "\")", sep = ""), sep = "\n")
+
+# all alias
+cat(paste("\\alias(", c("ascii", meth), ")", sep = ""), sep = "\n")
+
