0631811d402d142e4e1cf81d430ac3b2f9bfa0bf
David Hajage <david@david-laptop.(none)>
2009-05-13 16:55:53 +0200

    add possibility to write output of ascii() into a file

--- R/print.r
+++ R/print.r
@@ -1,8 +1,17 @@
 options(asciiType = "asciidoc")
 
-print.ascii <- function(x, type = getOption("asciiType"), ...) {
- if (type == "asciidoc") x$show.asciidoc()
- if (type == "t2t") x$show.t2t()
-#  if (type == "textile") x$show.textile()
+print.ascii <- function(x, type = getOption("asciiType"), file = NULL, append = FALSE, ...) {
+  if (is.null(file)) {
+    if (type == "asciidoc") x$show.asciidoc()
+    if (type == "t2t") x$show.t2t()
+  }
+  else {
+    if (type == "asciidoc") res <- capture.output(x$show.asciidoc())
+    if (type == "t2t") res <- capture.output(x$show.t2t())
+    if (append) op <- "a" else op <- "w"
+    f <- file(file, op)
+    writeLines(res, f)
+    close(f)
+  }
 }
 
