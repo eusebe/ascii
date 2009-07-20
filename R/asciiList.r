00966dea7dadc45cae8d0ec45ff13126ccd681a6
David Hajage <david@david-laptop.(none)>
2009-07-20 14:42:38 +0200

    ascii.simple.list (example : ascii(version))

--- R/asciiList.r
+++ R/asciiList.r
@@ -4,3 +4,10 @@ ascii.list <- function(x, caption = "", caption.level = ".", list.type = "bullet
     return(obj)
 }
 
+ascii.simple.list <- function(x, caption = "", caption.level = ".", list.type = "label", ...) {
+    x <- unlist(x)
+    obj <- asciiList$new(x = x, caption = caption, caption.level = caption.level, list.type = list.type)
+    class(obj) <- c("ascii", "proto", "environment")
+    return(obj)
+}
+
