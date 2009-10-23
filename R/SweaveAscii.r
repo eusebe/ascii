0f06bf0f27b0cce54e8e90e76230e7002ac3a231
David Hajage <david@crunchbang.(none)>
2009-10-23 15:52:28 +0200

    final change for 0.3 release

--- R/SweaveAscii.r
+++ R/SweaveAscii.r
@@ -1,4 +1,11 @@
-SweaveAscii <- Sweave
+Asciidoc <- Sweave
+formals(Asciidoc) <-  alist(file=, driver=RweaveAsciidoc, syntax=SweaveSyntaxNoweb, ...=)
 
-formals(SweaveAscii) <-  alist(file=, driver=RweaveAsciidoc, syntax=SweaveSyntaxNoweb, ...=)
+T2t <- Sweave
+formals(T2t) <-  alist(file=, driver=RweaveT2t, syntax=SweaveSyntaxNoweb, ...=)
 
+Sphinx <- Sweave
+formals(Sphinx) <-  alist(file=, driver=RweaveSphinx, syntax=SweaveSyntaxNoweb, ...=)
+
+Org <- Sweave
+formals(Org) <-  alist(file=, driver=RweaveOrg, syntax=SweaveSyntaxNoweb, ...=)
