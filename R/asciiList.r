284d8a6f710a9ca6f6de01bddf11278a4be43084
David Hajage <david@crunchbang.(none)>
2009-10-28 14:10:15 +0100

    add method for packageDescription

--- R/asciiList.r
+++ R/asciiList.r
@@ -11,3 +11,11 @@ ascii.simple.list <- function(x, caption = "", caption.level = "", list.type = "
     return(obj)
 }
 
+ascii.packageDescription <- function(x, caption = "", caption.level = "", list.type = "label", ...) {
+  x <- unclass(x)
+#  x$File <- attributes(x)$file
+  x <- lapply(x, function(x) gsub("\n", " ", x))
+  obj <- asciiList$new(x = x, caption = caption, caption.level = caption.level, list.type = list.type)
+    class(obj) <- c("ascii", "proto", "environment")
+    return(obj)  
+}
