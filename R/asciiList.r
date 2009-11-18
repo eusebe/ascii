de251cd42ed6cf9319605e1cbcff22d304b038dc
David Hajage <dhajage@gmail.com>
2009-11-18 11:05:11 +0100

    add 'escape' option to print.ascii

--- R/asciiList.r
+++ R/asciiList.r
@@ -16,8 +16,8 @@ ascii.packageDescription <- function(x, caption = "", caption.level = "", list.t
 #  x$File <- attributes(x)$file
   x <- lapply(x, function(x) gsub("\n", " ", x))
   obj <- asciiList$new(x = x, caption = caption, caption.level = caption.level, list.type = list.type)
-    class(obj) <- c("ascii", "proto", "environment")
-    return(obj)  
+  class(obj) <- c("ascii", "proto", "environment")
+  return(obj)  
 }
 
 ascii.sessionInfo <- function (x, locale = TRUE, ...) {
--- R/print.r
+++ R/print.r
@@ -1,17 +1,20 @@
 options(asciiType = "asciidoc")
 
-print.ascii <- function(x, type = getOption("asciiType"), file = NULL, append = FALSE, ...) {
+print.ascii <- function(x, type = getOption("asciiType"), file = NULL, append = FALSE, escape = FALSE, ...) {
+  if (type == "asciidoc") res <- capture.output(x$show.asciidoc())
+  if (type == "rest") res <- capture.output(x$show.rest())
+  if (type == "org") res <- capture.output(x$show.org())
+  if (type == "t2t") res <- capture.output(x$show.t2t())
+
+  if (escape) {
+    res <- gsub("\\_", "\\\\_", res)
+    res <- gsub("\\^", "\\\\^", res)
+  }
+  
   if (is.null(file)) {
-    if (type == "asciidoc") x$show.asciidoc()
-    if (type == "rest") x$show.rest()
-    if (type == "org") x$show.org()
-    if (type == "t2t") x$show.t2t()
+    cat(res, sep = "\n")
   }
   else {
-    if (type == "asciidoc") res <- capture.output(x$show.asciidoc())
-    if (type == "rest") res <- capture.output(x$show.rest())
-    if (type == "org") res <- capture.output(x$show.org())
-    if (type == "t2t") res <- capture.output(x$show.t2t())
     if (append) op <- "a" else op <- "w"
     f <- file(file, op)
     writeLines(res, f)
