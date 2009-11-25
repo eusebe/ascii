7de2716f6bc4eba94c78ac57fb32680660098895
David Hajage <dhajage@gmail.com>
2009-11-25 16:10:39 +0100

    format = 'f' in ascii.freqtable

--- R/asciiDescr.r
+++ R/asciiDescr.r
@@ -239,7 +239,7 @@ ascii.CrossTable <- function (x, ...) {
     return(res)
   }
 
-ascii.freqtable <- function (x, header = TRUE, footer = TRUE, digits = c(0, 4, 4), format = "fg", na.print = "", include.rownames = TRUE, include.colnames = TRUE, caption = x$label, ...) {
+ascii.freqtable <- function (x, header = TRUE, footer = TRUE, digits = c(0, 2, 2), format = "f", na.print = "", include.rownames = TRUE, include.colnames = TRUE, caption = x$label, ...) {
   res <- x$freqtable
   ascii(res, header = header, footer = footer, include.rownames = include.rownames, include.colnames = include.colnames, caption = caption, digits = digits, format = format, na.print = na.print, ...)
 }
