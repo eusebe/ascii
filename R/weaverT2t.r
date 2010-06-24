07a60c3372c4dff7f475ae6b1b0b2786fe704550
David Hajage <dhajage@gmail.com>
2010-06-24 16:32:45 +0200

    weaver based cache system for RweaveTextile, RweaveReST, RweaveOrg and RweaveT2t

---
+++ R/weaverT2t.r
@@ -0,0 +1,34 @@
+## Adapted from weaver package code by Seth Falcon
+
+weaverT2t <- function()
+{
+    require(weaver)
+    list(setup = weaverT2tSetup,
+         runcode = weaverT2tRuncode,
+         writedoc = RweaveT2tWritedoc,
+         finish = weaverT2tFinish,
+         checkopts = RweaveT2tOptions)
+}
+
+weaverT2tSetup <-
+    function(file, syntax, output=NULL, quiet=FALSE, debug=FALSE,
+             stylepath, use.cache=TRUE, ...)
+{
+    if (!quiet)
+      cat("Working dir:", getwd(), "\n")
+    log_debug(paste("Working dir:", getwd()))
+    res <- RweaveT2tSetup(file, syntax, output, quiet, debug, stylepath, use.cache, ...)
+    res$options[["use.cache"]] <- use.cache
+    res$options[["cache"]] <- FALSE
+    ## to be on the safe side: see if defaults pass the check
+    res$options <- RweaveT2tOptions(res$options)
+    res
+}
+
+weaverT2tFinish <- function(object, error=FALSE) {
+    resetStorage(cache_expr)
+    RweaveT2tFinish(object, error)
+}
+
+
+weaverT2tRuncode <- makeRweaveT2tCodeRunner(evalFunc=weaverEvalWithOpt)
---
+++ R/weaverTextile.r
@@ -0,0 +1,34 @@
+## Adapted from weaver package code by Seth Falcon
+
+weaverTextile <- function()
+{
+    require(weaver)
+    list(setup = weaverTextileSetup,
+         runcode = weaverTextileRuncode,
+         writedoc = RweaveTextileWritedoc,
+         finish = weaverTextileFinish,
+         checkopts = RweaveTextileOptions)
+}
+
+weaverTextileSetup <-
+    function(file, syntax, output=NULL, quiet=FALSE, debug=FALSE,
+             stylepath, use.cache=TRUE, ...)
+{
+    if (!quiet)
+      cat("Working dir:", getwd(), "\n")
+    log_debug(paste("Working dir:", getwd()))
+    res <- RweaveTextileSetup(file, syntax, output, quiet, debug, stylepath, use.cache, ...)
+    res$options[["use.cache"]] <- use.cache
+    res$options[["cache"]] <- FALSE
+    ## to be on the safe side: see if defaults pass the check
+    res$options <- RweaveTextileOptions(res$options)
+    res
+}
+
+weaverTextileFinish <- function(object, error=FALSE) {
+    resetStorage(cache_expr)
+    RweaveTextileFinish(object, error)
+}
+
+
+weaverTextileRuncode <- makeRweaveTextileCodeRunner(evalFunc=weaverEvalWithOpt)
