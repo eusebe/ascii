3b17b2993aa345d9830848ce36545752dff9b438
David Hajage <dhajage@gmail.com>
2010-08-25 12:17:36 +0200

    enable export to all formats supported by a2x

--- R/export.r
+++ R/export.r
@@ -7,30 +7,8 @@ section <- function(caption, caption.level = 2) {
 print.section <- function(x, type = "asciidoc", ...) {
   caption <- x$caption
   caption.level <- x$caption.level
-  
-  results <- NULL
-  if (!is.null(caption)) {
-    if (is.null(caption.level)) {
-      results <- paste(".", caption, "\n", results, sep = "")
-    } else if (caption.level == ".") {
-      results <- paste(".", caption, "\n", results, sep = "")
-    } else if (is.numeric(caption.level) & caption.level > 0) {
-      lev <- paste(rep("=", caption.level), collapse = "")
-      results <- paste(lev, " ", caption, " ", "\n\n", results, sep = "")
-    } else if (caption.level == "s") {
-      results <- paste(beauty.asciidoc(caption, "s"), "\n\n", results, sep = "")
-    }
-    else if (caption.level == "e") {
-      results <- paste(beauty.asciidoc(caption, "e"), "\n\n", results, sep = "")
-    }
-    else if (caption.level == "m") {
-      results <- paste(beauty.asciidoc(caption, "m"), "\n\n", results, sep = "")
-    }
-    else if (caption.level == "none" | caption.level == "") {
-      results <- paste(caption, "\n\n", results, sep = "")
-    }
-  }
-  cat(results, sep = "")
+  results <- header.asciidoc(caption, caption.level)
+  cat("\n", results, sep = "")
 }
 
 paragraph <- function(text, new = TRUE) {
@@ -56,15 +34,8 @@ sexpr <- function(x) {
 }
 
 print.sexpr <- function(x, ...) {
-  text <- paste("\\Sexpr{", x, "}", sep = "")
-  cat(text)
+  cat(x, "\n")
 }
-
-## footnote <- function(text) {
-
-##   class(results) <- "footnote"
-##   results  
-## }
 
 convert <- function(input, destination = NULL, format = "xhtml", encoding = NULL, cmd = NULL) {
 
@@ -89,34 +60,50 @@ convert <- function(input, destination = NULL, format = "xhtml", encoding = NULL
       destination <- "."
     }
     if (format != "xhtml") {
-      cmd <- paste("a2x -a encoding=", encoding, " -D ", destination, " -f ", format, sep = "")
+      cmd <- paste("a2x -a encoding=", encoding, " -D ", destination, " --dblatex-opts=\"-T simple\"", " -f ", format, sep = "")
     } else {
       destfile <- paste(paste(destination, sub("^(.+)(.txt)$", "\\1", basename(input)), sep = "/"), "html", sep = ".")
       cmd <- paste("asciidoc -a encoding=", encoding, " -o ", destfile, sep = "")
     }
     
   }
-  cmd <- paste(cmd, input, closecmd)
-  system(cmd, wait = TRUE)
+  finalcmd <- paste(cmd, input, closecmd)
+  err <- system(finalcmd, wait = TRUE)
+  invisible(err)
 }
 
-export <- function(..., file = NULL, destination = NULL, format = "xhtml", open = TRUE, main = NULL, author = NULL, email = NULL, revdate = NULL, revnumber = NULL, cmd = NULL, encoding = NULL) {
-  if (is.null(file)) {
-    file <- tempfile("asciidoc-report")
-  }
-  if (is.null(main)) {
-    main <- file
+export <- function(..., file = NULL, destination = NULL, format = "xhtml", open = NULL, main = NULL, author = NULL, email = NULL, revdate = NULL, revnumber = NULL, cmd = NULL, encoding = NULL) {
+
+  format <- format[1]
+  available <- c("chunked", "epub", "htmlhelp", "pdf", "text", "xhtml", "dvi", "ps", "tex", "docbook")
+  if (!(format %in% available)) {
+    stop(paste("Please choose an available format:", paste(available, collapse = ", ")))
   }
 
+  if (is.null(file)) {
+    file <- tempfile("R-report")
+    if (is.null(open)) {
+      open <- TRUE
+    }
+  } else {
+    if (is.null(open)) {
+      open <- FALSE
+    }
+  }
+  
   if (is.null(destination)) {
     wd <- dirname(file)
   } else {
     wd <- destination
   }
-  
+
+  if (is.null(main)) {
+    main <- paste(wd, basename(file), sep = "/")
+  }
+
   args <- list(...)
   lines <- capture.output({
-                          print(section(main, 1))
+                          cat(paste("= ", main, "\n", sep = ""))
                           if (!is.null(author)) {
                             cat(":author:", author, "\n")
                           }
@@ -124,14 +111,20 @@ export <- function(..., file = NULL, destination = NULL, format = "xhtml", open 
                             cat(":email:", email, "\n")
                           }
                           if (!is.null(revdate)) {
-                            cat(":revate:", revdate, "\n")
+                            cat(":revdate:", revdate, "\n")
                           }
                           if (!is.null(revnumber)) {
                             cat(":revnumber:", revnumber, "\n")
                           }
+                          cat("\n")
                           for (i in seq_along(args)) {
                             arg <- args[[i]]
                             if ("ascii" %in% class(arg)) {
+                              if (!is.null(names(args))) {
+                                 if (names(args)[i] != "") {
+                                   cat(".", names(args)[i], "\n", sep = "")
+                                 }
+                               }
                               arg$show.asciidoc()
                               cat("\n")
                             } else {
@@ -145,11 +138,58 @@ export <- function(..., file = NULL, destination = NULL, format = "xhtml", open 
 
   if (format == "xhtml") {
     htmlfile <- paste(basename(file), "html", sep = ".")
-    convert(textfile, destination = wd, format = "xhtml", cmd = cmd, encoding = encoding)
-    if (open) {
-      browseURL(paste(wd, htmlfile, sep = "/"))
+    finalfile <- paste(wd, htmlfile, sep = "/")
+  }
+  if (format == "chunked") {
+    finalfile <- paste(wd, paste(basename(file), "chunked", sep = "."), "index.html", sep = "/")
+  }
+  if (format == "htmlhelp") {
+    finalfile <- paste(wd, paste(basename(file), "htmlhelp", sep = "."), "index.html", sep = "/")
+  }
+  if (format == "pdf") {
+    pdffile <- paste(basename(file), "pdf", sep = ".")
+    finalfile <- paste(wd, pdffile, sep = "/")
+  }
+  if (format == "text") {
+    txtfile <- paste(basename(file), "text", sep = ".")
+    finalfile <- paste(wd, txtfile, sep = "/")
+  }
+  if (format == "dvi") {
+    dvifile <- paste(basename(file), "dvi", sep = ".")
+    finalfile <- paste(wd, dvifile, sep = "/")
+  }
+  if (format == "ps") {
+    psfile <- paste(basename(file), "ps", sep = ".")
+    finalfile <- paste(wd, psfile, sep = "/")
+  }
+  if (format == "tex") {
+    texfile <- paste(basename(file), "tex", sep = ".")
+    finalfile <- paste(wd, texfile, sep = "/")
+  }
+  if (format == "epub") {
+    epubfile <- paste(basename(file), "epub", sep = ".")
+    finalfile <- paste(wd, epubfile, sep = "/")
+  }
+  if (format == "docbook") {
+    xmlfile <- paste(basename(file), "xml", sep = ".")
+    finalfile <- paste(wd, xmlfile, sep = "/")
+  }
+
+  cat("Writing ", finalfile, "...\n", sep = "")
+  err <- convert(textfile, destination = wd, format = format, cmd = cmd, encoding = encoding)
+  if (err != 0 ) {
+    stop("Error during conversion.")
+  }
+  cat("Done\n")
+
+  if (open) {
+    cat("Trying to open ", finalfile, sep = "")
+    if (grepl("mingw", version$os)) {
+      cat(" with shell.exec...\n")
+      shell.exec(finalfile)
+    } else {
+      cat(" with xdg-open...\n")
+      system(paste(shQuote("/usr/bin/xdg-open"), shQuote(finalfile)), wait = FALSE, ignore.stderr = TRUE)
     }
   }
 }
-
-file = NULL; format = "html"; open = TRUE; main = NULL; author = NULL; email = NULL; revdate = NULL; revnumber = NULL; cmd = NULL; encoding = NULL
