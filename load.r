70a3a988e95f72caa98eb221ce1bc9d6e58cd097
David Hajage <dhajage@gmail.com>
2011-04-16 10:29:39 +0200

    update load.r

--- load.r
+++ load.r
@@ -1,2 +1,7 @@
 listoffiles <- paste("R/", list.files("R", "^.+\\.r$"), sep = "")
 for (i in listoffiles) source(i)
+
+source("R/RweaveAscii_R2.13.r")
+source("R/weaver_R2.13.r")
+source("R/cacheSweaveAscii.r")
+source("R/cacheSweaveDrivers.r")
