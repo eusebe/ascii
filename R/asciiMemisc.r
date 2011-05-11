e169ecf934f28e32c1c4fbad9a23cf9cc5a20f5b
David Hajage <dhajage@gmail.com>
2011-05-11 11:04:53 +0200

    add method for mtable objects (memisc package)

---
+++ R/asciiMemisc.r
@@ -0,0 +1,31 @@
+##' ascii method for class mtable
+##'
+##' \code{memisc} package
+##' @param x An R object of class found among \code{methods(ascii)}.
+##' @param lgroup Character vector or list of character vectors defining major
+##'   left row headings.  The default is to have none (\code{NULL}).
+##' @param n.lgroup A numeric vector containing the number of rows for which
+##'   each element in lgroup is a heading. Column names count in the row
+##'   numbers if \code{include.colnames = TRUE}.
+##' @param include.rownames logical. If \code{TRUE} the rows names are printed.
+##'   Default value depends of class of \code{x}.
+##' @param include.colnames logical. If \code{TRUE} the columns names are
+##'   printed. Default value depends of class of \code{x}.
+##' @param ... Additional arguments.  (see \code{?ascii.default}).
+##' @return An ascii object.
+##' @export
+##' @method ascii memisc
+##' @author David Hajage
+##' @examples
+##' library(memisc)
+##' lm0 <- lm(sr ~ pop15 + pop75,              data = LifeCycleSavings)
+##' lm1 <- lm(sr ~                 dpi + ddpi, data = LifeCycleSavings)
+##' lm2 <- lm(sr ~ pop15 + pop75 + dpi + ddpi, data = LifeCycleSavings)
+##' x <- mtable("Model 1"=lm0,"Model 2"=lm1,"Model 3"=lm2)
+##' ascii(x)
+ascii.mtable <- function(x, lgroup = c(dimnames(x$coefficients)[[3]], rownames(x$summaries)), n.lgroup = c(rep(2, dim(x$coefficients)[x$as.row[2]]), rep(1, nrow(x$summaries))), include.rownames = FALSE, include.colnames = TRUE, ...) {
+  coefs <- ftable(as.table(x$coefficients), row.vars = rev(x$as.row), col.vars = rev(x$as.col))
+  coefs <- rbind(coefs, x$summaries)
+
+  ascii.default(coefs, lgroup = lgroup, n.lgroup = n.lgroup, include.rownames = include.rownames, include.colnames = include.colnames, ...)
+}
